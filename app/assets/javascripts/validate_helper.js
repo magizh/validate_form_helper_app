396c86ad46cd3d93d7a08697d412490bcd082fc3
Magi <mahilis@live.com>
2012-05-29 23:25:59 -0700

    modified helper

---
+++ app/assets/javascripts/validate_helper.js
@@ -0,0 +1,48 @@
+; !function( $ ){
+$.fn.extend({
+validate: function( callback ) {
+
+        // Checks if instance is already created 
+        if ( this.data("instance") ) {
+          return;
+        }
+
+        var $this = this
+        var temp=[]
+
+        $this.find(".validate").each(function (i, el) {
+          var $el = $(el)
+          temp.push({name: $el.attr("name"), 
+                     rules: $el.attr("data-rules"),
+                     display: $el.attr("data-display")
+                   })
+        })
+        // Create FormValidator object
+        var validator = new FormValidator($this.attr('name'), temp, function(errors, event) {
+          // Clear all error fields
+          $this.find(".error").removeClass("error")
+          $this.find(".help-block").html("")
+          // Check for errors 
+          if(errors.length > 0) {
+              $.each(errors, function (index, err) {
+                // Displays the erros message in the help-block
+                var $target = $this
+                    .find("*[name='" + err.id + "']")
+                    .next(".help-block")
+                    .html("<i src='/img/close.png'></i>"+err.message)
+                // Adds error class to the controlgroup (bootstrap)
+                $target.closest(".control-group").removeClass("error").addClass("error")
+              })
+             return false 
+          } else {
+            if (typeof callback === "function") {
+              callback ($this, event ) //execute callback on form success
+            }
+          }
+        });
+        this.data("instance", validator)
+      }
+
+      })
+
+} ( window.jQuery )
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ValidateFormHelperApp::Application.config.secret_token = '9a837f4d73a7af679c35aad93a2aff023e62e752a21cf35094a151468bf57ea3264bb1e67f814346d4351e3d5b14f3da5c8f2edeefe32c7263c052245ea6e36b'
